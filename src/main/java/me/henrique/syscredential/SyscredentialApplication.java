5ffc7a313d6601bd212a7fa6ba9780e08d09114c
Carlos Henrique S. Ferreira <ch.ferreir@hotmail.com>
2020-08-17 22:29:41 -0300

    setup inicial

---
+++ src/main/java/me/henrique/syscredential/SyscredentialApplication.java
@@ -0,0 +1,13 @@
+package me.henrique.syscredential;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyscredentialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyscredentialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/henrique/syscredential/SyscredentialApplicationTests.java
@@ -0,0 +1,13 @@
+package me.henrique.syscredential;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyscredentialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
