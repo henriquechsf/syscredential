5ba79431fce3411d125793750e2301b37383e4fc
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-01-04 19:24:12 -0300

    restringe cadastro e edição de Usuarios somente para perfis ADMIN

--- src/main/java/me/henrique/syscredential/controller/UsuarioController.java
+++ src/main/java/me/henrique/syscredential/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import me.henrique.syscredential.domain.services.GestaoUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -40,6 +41,7 @@ public class UsuarioController {
 		return ResponseEntity.ok(new UsuarioResponse(usuario));
 	}
 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<UsuarioResponse> adicionar(@Valid @RequestBody UsuarioRequest dto) {
