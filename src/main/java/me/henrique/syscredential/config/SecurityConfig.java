56c6490bf797397a6dcfdb75f05b397d592efedf
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-05-01 21:31:02 -0300

    remove autenticação para testes

--- src/main/java/me/henrique/syscredential/config/SecurityConfig.java
+++ src/main/java/me/henrique/syscredential/config/SecurityConfig.java
@@ -34,12 +34,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String[] PUBLIC_MATCHERS = {
             "/h2-console/**",
             "/login/**",
-            "/usuarios/**"
+            "/usuarios/**",
+            "/participantes/**",
+            "/eventos/**",
+            "/atividades/**",
+            "/regionais/**"
     };
 
     // somente leitura
     private static final String[] PUBLIC_MATCHERS_GET = {
-            "/eventos/**"
+            //"/eventos/**"
     };
 
     @Override
@@ -50,12 +54,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         }
 
         http.cors().and().csrf().disable();
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .anyRequest().authenticated();
+
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
         http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
